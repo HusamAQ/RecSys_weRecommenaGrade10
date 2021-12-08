ccb8087bfe7dde814b1832f349496fd8a463a2f8
Pascal <p.anema@student.maastrichtuniversity.nl>
2021-12-09 00:36:11 +0100

    small addition

--- we_recommend_a_grade_10/methods.py
+++ we_recommend_a_grade_10/methods.py
@@ -6,18 +6,18 @@ from lenskit.metrics.predict import user_metric, rmse
 from we_recommend_a_grade_10.data import *
 
 
-def get_train_test_split(split_count=6.9, train_size=0.7, seed=None):
-    ratings_per_user = simple_Ratings[['user', 'item']].groupby(['user']).count()
+def get_train_test_split(ratings=simple_Ratings, split_count=6.9, train_size=0.7, seed=None):
+    ratings_per_user = ratings[['user', 'item']].groupby(['user']).count()
     ratings_per_user = ratings_per_user.reset_index()
     users_more_than = ratings_per_user[ratings_per_user['item'] > split_count]
 
     # select a sample of int(split_count) ratings from each user in users_more_than,
     #  this is our training set
     # what remains of the ratings is the testing set
-    train_set = pd.DataFrame(columns=simple_Ratings.columns)
-    test_set = pd.DataFrame(columns=simple_Ratings.columns)
+    train_set = pd.DataFrame(columns=ratings.columns)
+    test_set = pd.DataFrame(columns=ratings.columns)
     for user in users_more_than.user:
-        user_ratings = simple_Ratings[simple_Ratings['user'] == user]
+        user_ratings = ratings[ratings['user'] == user]
         sample_size = len(user_ratings) * train_size
         sample = user_ratings.sample(int(sample_size), random_state=seed)
 
